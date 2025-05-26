d15bc795f7447b3a69afad6e8ad7ea6dadba8055
Nazariy Kazaryan <150127326+laRataAlada19@users.noreply.github.com>
2025-05-26 23:49:50 +0100

    2 dockerfiles, 2 modules functioning

--- consumer/src/test/java/com/wit/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/wit/consumer/ConsumerApplicationTests.java
@@ -2,8 +2,10 @@ package com.wit.consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.kafka.listener.auto-startup=false")
 class ConsumerApplicationTests {
 
 	@Test
